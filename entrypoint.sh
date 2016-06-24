81fceea48c28bb1ac5daccd69dd26a4319ec7abb
Andrew Grigorev <andrew@ei-grad.ru>
2016-06-24 17:11:56 +0300

    Fix permission

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cat /etc/newrelic-plugin-agent.cfg.tpl | envsubst > /etc/newrelic-plugin-agent.cfg
+cat /etc/newrelic-plugin-agent.cfg.tpl | envsubst > /home/user/newrelic-plugin-agent.cfg
 
-exec newrelic-plugin-agent -c /etc/newrelic-plugin-agent.cfg
+exec newrelic-plugin-agent -c /home/user/newrelic-plugin-agent.cfg
